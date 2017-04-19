8649b9c35ade7b62647c89d3db025184b71a34fb
amara <amara@Pieshin>
2017-04-19 15:29:34 +0900

    fwef

--- JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -6,26 +6,38 @@ package org.dimigo.oop;
 /**
  * <pre>
  * org.dimigo.oop
- *  |_ PiggyBank
+ *  |_ PiggyBankTest
  *
  * 1. 개요 :
  * 2. 작성일 : 2017. 4. 19.
  * </pre>
  *
- * @author :amara
+ * @autho
  * @version : 1.0
  *
  */
-public class PiggyBank {
-   private static int balance;
-   
-   public static void putMoney(FamilyMember member, int amount){
-      System.out.println(member.getMemberName()+ ":" +amount+"원 넣음");
-      balance+=amount;
+public class PiggyBankTest {
+
+   public static void main(String[] args) {
+      
+      FamilyMember[] member = {
+            new FamilyMember("아빠"),
+            new FamilyMember("엄마"),
+            new FamilyMember("나"),
+            new FamilyMember("남동생")
+      };
+      FamilyMember.printMemberCnt();
+      
+      PiggyBank.putMoney(member[0], 10000);
+      PiggyBank.putMoney(member[1], 5000);
+      PiggyBank.putMoney(member[2], 2000);
+      PiggyBank.putMoney(member[3], 1000);
+      
+      PiggyBank.printBalance();
+      
+      PiggyBank.putMoney(member[2], 1000);
+      PiggyBank.printBalance();
+
    }
 
-   
-   public static void printBalance(){
-      System.out.println("돼지저금통 총 금액 : "+balance+"원");
-   }
 }
